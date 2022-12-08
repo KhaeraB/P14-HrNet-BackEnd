5d5d99f5c51cc0dfcc562cf53eff27e3fcdf4bf7
BELKADI <khaera/belkadi@gmail.com>
2022-12-09 00:47:46 +0100

    vercel npm

--- server.js
+++ server.js
@@ -1,61 +1,60 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const path = require("path");
-const { logger, logEvents } = require("./middleware/logger");
-const errorHandler = require("./middleware/errorHandler");
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const path = require('path')
+const {logger, logEvents} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser')
 
-const connectDB = require("./config/dbConnect");
-const PORT = process.env.PORT || 3001;
+const connectDB = require('./config/dbConnect')
+const PORT = process.env.PORT || 3001
 // console.log(process.env.NODE_ENV)
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE,");
-  next();
-});
 
-app.use(logger);
+ app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE,');
+    next();
+  });
 
-app.use(express.json());
+app.use(logger)
 
-app.use(cookieParser());
+app.use(express.json())
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 //endPoints
-app.use("/", require("./routes/root"));
-app.use("/employees", require("./routes/employeesRoutes"));
+app.use('/', require('./routes/root'))
+app.use('/employees', require('./routes/employeesRoutes'))
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("text").send("404 Not Found");
-  }
-});
+app.all('*', (req, res)=>{
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }else if (req.accepts('json')){
+        res.json({message : '404 Not Found'})
+    }else{
+        res.type('text').send('404 Not Found')
+    }
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
 
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
-});
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
+
+
+
+
