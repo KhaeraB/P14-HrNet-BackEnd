989c42cf4ced55e88b8af9d712a66899952d7a89
BELKADI <khaera/belkadi@gmail.com>
2022-12-09 01:38:26 +0100

    test

--- server.js
+++ server.js
@@ -26,26 +26,14 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-// app.use('/', express.static(path.join(__dirname, 'public')))
 //home
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello World !!");
 })
 
 //endPoints
-// app.use('/', require('./routes/root'))
+app.use('/', require('./routes/root'))
 app.use('/employees', require('./routes/employeesRoutes'))
-
-app.all('*', (req, res)=>{
-    res.status(404)
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    }else if (req.accepts('json')){
-        res.json({message : '404 Not Found'})
-    }else{
-        res.type('text').send('404 Not Found')
-    }
-})
 
 app.use(errorHandler)
 
